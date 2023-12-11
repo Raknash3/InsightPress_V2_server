d2c2810557118c5c95e5bbc2a4562f4408ad8a78
Ravi Shankar <ivarraknash3@gmail.com>
2023-12-10 20:07:02 -0500

    Cors policy changed

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
