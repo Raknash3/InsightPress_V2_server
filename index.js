0d4bb47fa48b322bd404d64d0fed509fdf6d8e5e
Ravi Shankar <ivarraknash3@gmail.com>
2023-12-10 21:26:16 -0500

    Check render backend

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-    cors({
+app.use(cors(
+    {
+        origin: "https://famous-cannoli-ae582e.netlify.app/",
         credentials: true,
-        origin: process.env.FRONTEND_URL
     })
 );
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
